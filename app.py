c12d71ce2f72ad8b445718299b65ef65f6815720
Faisal <phesalbarwez@gmail.com>
2026-09-14 19:12:05 +0300

    feat: add /version endpoint

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ import os
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+APP_VERSION = "1.0.0"
 
 @app.route("/health")
 def health():
     return jsonify({"status": "UP"}), 200
+
+@app.route("/version")
+def version():
+    return jsonify({"version": APP_VERSION}), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
